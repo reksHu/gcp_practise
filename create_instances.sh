09db117b1ebdcaf1f381fd63d8e5e202f9dd41bf
reksHu <32813360+reksHu@users.noreply.github.com>
2021-01-18 18:19:19 +0800

    Add files via upload
    updated script

--- create_instances.sh
+++ create_instances.sh
@@ -1,35 +1,20 @@
 #!/bin/bash
 
-gcloud compute instances create www1 \
-  --image-family debian-9 \
-  --image-project debian-cloud \
-  --zone us-central1-a \
-  --tags network-lb-tag \
-  --metadata startup-script="#! /bin/bash
+gcloud compute instances create www1 --image-family debian-9 --image-project debian-cloud --zone us-central1-a --tags network-lb-tag --metadata startup-script="#! /bin/bash
     sudo apt-get update
     sudo apt-get install apache2 -y
     sudo service apache2 restart
     echo '<!doctype html><html><body><h1>www1</h1></body></html>' | tee /var/www/html/index.html"
 
 
-gcloud compute instances create www2 \
-  --image-family debian-9 \
-  --image-project debian-cloud \
-  --zone us-central1-a \
-  --tags network-lb-tag \
-  --metadata startup-script="#! /bin/bash
+gcloud compute instances create www2 --image-family debian-9 --image-project debian-cloud --zone us-central1-a --tags network-lb-tag --metadata startup-script="#! /bin/bash
     sudo apt-get update
     sudo apt-get install apache2 -y
     sudo service apache2 restart
     echo '<!doctype html><html><body><h1>www2</h1></body></html>' | tee /var/www/html/index.html"
 
 
-gcloud compute instances create www3 \
-  --image-family debian-9 \
-  --image-project debian-cloud \
-  --zone us-central1-a \
-  --tags network-lb-tag \
-  --metadata startup-script="#! /bin/bash
+gcloud compute instances create www3 --image-family debian-9 --image-project debian-cloud --zone us-central1-a --tags network-lb-tag --metadata startup-script="#! /bin/bash
     sudo apt-get update
     sudo apt-get install apache2 -y
     sudo service apache2 restart
